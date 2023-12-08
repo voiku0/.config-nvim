1b4135a6b9ac4cd95e0a956020e12647e61754c5
Tudor Voicu Bradu <tvbradu@gmail.com>
2023-12-08 22:55:19 +0200

    nvim

---
+++ nvim/lua/voiku/init.lua
@@ -0,0 +1,9 @@
+require("voiku.remap")
+require("voiku.packer")
+require("voiku.set")
+
+-- vim.api.nvim_set_hl(0, 'FloatBorder', {bg='#3B4252', fg='#5E81AC'})
+-- vim.api.nvim_set_hl(0, 'NormalFloat', {bg='#3B4252'})
+-- vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='#3B4252'})
+-- vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='#3B4252'})
+
